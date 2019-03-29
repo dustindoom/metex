5fbd8b56c50bbea684fc8adc01e77b1dbbb2467c
dustindoom <dustin@eltoro.com>
2019-03-29 11:55:03 -0400

    Added test files for `coordinator` and `worker`

---
+++ test/coordinator_test.exs
@@ -0,0 +1,7 @@
+defmodule Metex.CoordinatorTest do
+  use ExUnit.Case
+  doctest Metex.Coordinator
+
+  test "" do
+  end
+end
---
+++ test/worker_test.exs
@@ -0,0 +1,7 @@
+defmodule Metex.WorkerTest do
+  use ExUnit.Case
+  doctest Metex.Worker
+
+  test "" do
+  end
+end
